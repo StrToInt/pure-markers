7d80264577aad7e16da6d18ffd53cdfa813631da
strtoint <strtoint@mail.ru>
2017-06-29 15:08:21 +1000

    initial

---
+++ markers.js
@@ -0,0 +1,237 @@
+// Add your own markers to this file.
+
+var MAPCRAFTER_MARKERS = [
+	
+	// just one example marker group
+	{
+		// id of the marker group, without spaces/other special chars
+		"id" : "homes",
+		// name of the marker group, displayed in the webinterface
+		"name" : "Дома",
+		// icon of the markers belonging to that group (optional)
+		"icon" : "home.png",
+		// size of that icon
+		"iconSize" : [32, 32],
+		// whether this marker group is shown by default (optional)
+		"showDefault" : true,
+		"markers" : {
+			"pure" : [
+				{"pos" : [-91, 390, 64], "title" : "Дом 1523132/Звезда смерти"},
+				{"pos" : [-267, 520, 64], "title" : "Дом WaterDemon-a"},
+				{"pos" : [-434, 532, 64], "title" : "Особняк WaterDemon-a"},
+				{"pos" : [-100, 551, 64], "title" : "Дом SadMoon"},
+				{"pos" : [-10, 385, 64], "title" : "Дом leovolk"},
+				{"pos" : [-16, 442, 64], "title" : "Банк leovolk"},
+				{"pos" : [-327, 597, 64], "title" : "Дом HISENBERG-a"},
+				{"pos" : [452, 238, 64], "title" : "Маяк Morkovka228"},
+				{"pos" : [11903, 20969, 64], "title" : "Дом tur1k-a"},
+				{"pos" : [730, 1786, 64], "title" : "Дом Sun_Serega"},
+				{"pos" : [-248, 360, 64], "title" : "Дом AT13"},
+				{"pos" : [7300, 740, 64], "title" : "Дом Dragon165"},
+				{"pos" : [-150, 315, 64], "title" : "Дом Serfing-a"},
+				{"pos" : [-160, 385, 64], "title" : "Дом arhitector-a"},
+				{"pos" : [-71, 459, 64], "title" : "Макдак AlanFox-a"},
+				{"pos" : [-122, 502, 64], "title" : "Дом Lignium-a"},
+				{"pos" : [-151, 512, 64], "title" : "Первый общий дом"},
+				{"pos" : [-529, 1364, 64], "title" : "Замок Mars129"},
+				{"pos" : [-1696, 2437, 64], "title" : "Дом Crasis"},
+				{"pos" : [-1683, 2262, 64], "title" : "Маяк Crasis"},
+				{"pos" : [-238, 406, 64], "title" : "Дом ostapchikus-a"},
+				{"pos" : [-162, 436, 64], "title" : "Новый Общий дом"},
+				{"pos" : [-121, 472, 64], "title" : "Магазин Sun_Serega"},
+				{"pos" : [116, 332, 64], "title" : "Лошадиное ранчо Morkovka228"},
+				{"pos" : [-40, 497, 64], "title" : "Дом чечЕнки от arhitector-a"},
+				{"pos" : [-95, 448, 64], "title" : "Дом AlanFox-a"},
+				{"pos" : [-321, 224, 64], "title" : "Общее ранчо"},
+				{"pos" : [-11991, 187, 64], "title" : "Дом XpeHoK"},
+				{"pos" : [-456, 631, 64], "title" : "Дом Borsk-a"}
+
+			],
+		},
+	},
+	
+	{
+		// id of the marker group, without spaces/other special chars
+		"id" : "farms",
+		// name of the marker group, displayed in the webinterface
+		"name" : "Фермы",
+		// icon of the markers belonging to that group (optional)
+		"icon" : "farm.png",
+		// size of that icon
+		"iconSize" : [32, 32],
+		// whether this marker group is shown by default (optional)
+		"showDefault" : true,
+		// markers of this marker group...
+		"markers" : {
+			// ...in the world "world"
+			"pure" : [
+				{"pos" : [-372, 391, 64], "title" : "Мобоферма"},
+				{"pos" : [310, 489, 64], "title" : "Мобоферма Kapiteon-a"},
+				{"pos" : [302, 690, 64], "title" : "Ферма ведьм Kapiteon-a"},
+				{"pos" : [-306, 713, 64], "title" : "Ферма скелетов"},
+				{"pos" : [-18, 230, 64], "title" : "Ферма Железа"},
+				{"pos" : [-1822, 603, 64], "title" : "Ферма Ведьм"},
+				{"pos" : [-198, -1250, 64], "title" : "Ферма стражей"},
+				{"pos" : [175, 517, 64], "title" : "Магазин жителей"},
+				{"pos" : [163, 559, 64], "title" : "Склад фермы тростника, тыквы и арбузов"},
+				{"pos" : [215, 587, 64], "title" : "Ферма пшеницы"},
+				{"pos" : [178, 637, 64], "title" : "Ферма булыжника"},
+				{"pos" : [644,1816, 64], "title" : "Ферма корусов Sun_Serega"},
+				{"pos" : [801,2500, 64], "title" : "Ферма еды Sun_Serega"},
+				{"pos" : [-3593,499, 64], "title" : "Ферма изумрудов Sun_Serega"},
+				{"pos" : [649,400, 64], "title" : "Ферма тростника"}
+
+			],
+			"nether" : [
+				{"pos" : [231, -161, 64], "title" : "Ферма скелетов иссушителей"},
+				{"pos" : [-18, 203, 64], "title" : "Ферма опыта свинозомби"},
+
+			],
+			"nether_caves" : [
+				{"pos" : [231, -161, 64], "title" : "Ферма скелетов иссушителей"}
+
+			],
+			"pure_end" : [
+				{"pos" : [1, 5, 64], "title" : "Ферма (дюпалка) песка"},
+				{"pos" : [309, 57, 64], "title" : "Ферма жемчуга"},
+
+			],
+		},
+	},
+	{
+		// id of the marker group, without spaces/other special chars
+		"id" : "portals",
+		// name of the marker group, displayed in the webinterface
+		"name" : "Порталы",
+		// icon of the markers belonging to that group (optional)
+		"icon" : "portal.png",
+		// size of that icon
+		"iconSize" : [32, 32],
+		// whether this marker group is shown by default (optional)
+		"showDefault" : true,
+		// markers of this marker group...
+		"markers" : {
+			// ...in the world "world"
+			"pure" : [
+				{"pos" : [-47, 232, 64], "title" : "Спавн", "icon" : "spawn.png"},
+				{"pos" : [-37, 225, 64], "title" : "Спавн"},
+				{"pos" : [720, 1780, 64], "title" : "Портал в край"},
+				{"pos" : [-164, 467, 64], "title" : "Общая деревня, портал в ад"},
+				{"pos" : [173, 529, 64], "title" : "Фарм зона"}
+
+			],
+			"nether" : [
+				{"pos" : [-22, 68, 64], "title" : "Хаб", "icon" : "spawn.png"},
+				{"pos" : [-21, 65, 64], "title" : "Общая деревня"},
+				{"pos" : [96, 222, 64], "title" : "Дом Sun_Serega"},
+				{"pos" : [1497, 2627, 64], "title" : "Дом tur1k"},
+				{"pos" : [907, 85, 64], "title" : "Дом Dragon165"},
+				{"pos" : [-33, -148, 64], "title" : "Ферма стражей"},
+				{"pos" : [-362, 411, 64], "title" : "Грибной биом"},
+				{"pos" : [239, -376, 64], "title" : "МЕСА"},
+				{"pos" : [-187, -477, 64], "title" : "Биом плотного льда"},
+				{"pos" : [-224, 71, 64], "title" : "Ферма изумрудов Sun_Sereg-и"}
+
+			],
+			"nether_caves" : [
+				{"pos" : [-22, 68, 64], "title" : "Хаб", "icon" : "spawn.png"},
+				{"pos" : [-21, 65, 64], "title" : "Общая деревня"},
+				{"pos" : [96, 222, 64], "title" : "Дом Sun_Serega"},
+				{"pos" : [1497, 2627, 64], "title" : "Дом tur1k"},
+				{"pos" : [907, 85, 64], "title" : "Дом Dragon165"},
+				{"pos" : [-33, -148, 64], "title" : "Ферма стражей"},
+				{"pos" : [-362, 411, 64], "title" : "Грибной биом"},
+				{"pos" : [239, -376, 64], "title" : "МЕСА"},
+				{"pos" : [-187, -477, 64], "title" : "Биом плотного льда"},
+				{"pos" : [-224, 71, 64], "title" : "Ферма изумрудов Sun_Sereg-и"}
+
+			],
+			"pure_end" : [
+				{"pos" : [0, 0, 64], "title" : "Спавн", "icon" : "spawn.png"},
+
+			],
+		},
+	},
+	{
+		// id of the marker group, without spaces/other special chars
+		"id" : "poi",
+		// name of the marker group, displayed in the webinterface
+		"name" : "Интересное",
+		// icon of the markers belonging to that group (optional)
+		"icon" : "poi.png",
+		// size of that icon
+		"iconSize" : [32, 32],
+		// whether this marker group is shown by default (optional)
+		"showDefault" : true,
+		// markers of this marker group...
+		"markers" : {
+			// ...in the world "world"
+			"pure" : [
+				{"pos" : [-2900,3300, 64], "title" : "Грибной биом"},
+				{"pos" : [20266,20017, 64], "title" : "Грибной биом"},
+				{"pos" : [1380,-2570, 64], "title" : "МЕСА"},
+				{"pos" : [-1461, -3254, 64], "title" : "Плотный лед"},
+				{"pos" : [328, 998, 64], "title" : "Старая ферма слизней"},
+				{"pos" : [-701, 1565, 64], "title" : "Ферма Legio"},
+				{"pos" : [-764, 1543, 64], "title" : "Дом Legio"},
+				{"pos" : [-85, 833, 64], "title" : "Недострой Nekro"},
+				{"pos" : [308, 255, 64], "title" : "Недострой Morkovka228"},
+				{"pos" : [138, 908, 64], "title" : "Ферма изумрудов San_Serega (Недострой)"},
+				{"pos" : [597, 709, 64], "title" : "Ферма ведьм. Хз кто строил..."},
+				{"pos" : [-764, 1338, 64], "title" : "Хз чей дом"},
+				{"pos" : [-422, 404, 64], "title" : "Коробка чъя-то"},
+				{"pos" : [-215, 218, 64], "title" : "Недострой на утесе"},
+				{"pos" : [-179, 192, 64], "title" : "Огород"},
+				{"pos" : [-247, 333, 64], "title" : "Руины"},
+				{"pos" : [476, 1223, 64], "title" : "Место добычи песка"},
+				{"pos" : [45, 723, 64], "title" : "Бревна на земле WTF???"},
+				{"pos" : [1306, 2611, 64], "title" : "Фермы куриц ??"},
+				{"pos" : [1441, 2645, 64], "title" : "Биом цветочного леса"},
+				{"pos" : [-111, 337, 64], "title" : "Сплиф"},
+				{"pos" : [-475, 2544, 64], "title" : "Новая деревня"},
+				{"pos" : [309, -1265, 64], "title" : "Недострой Sun_Sereg-и - \"Ферма пассивных мобов\""},
+
+			],
+			"nether" : [
+				{"pos" : [-29,-81, 64], "title" : "Строения mrdaft"},
+
+			],
+		},
+	},
+	// you can also add more complicated markers using the Leaflet API
+	// just specify a function which creates the Leaflet API marker objects
+	/*{
+		"id" : "test",
+		"name" : "Test",
+		"createMarker" : function(ui, groupInfo, markerInfo) {
+			var latlngs = [];
+			// use the ui.mcToLatLng-function to convert Minecraft coords to LatLngs
+			latlngs.push(ui.mcToLatLng(markerInfo.p1[0], markerInfo.p1[1], 64));
+			latlngs.push(ui.mcToLatLng(markerInfo.p2[0], markerInfo.p2[1], 64));
+			latlngs.push(ui.mcToLatLng(markerInfo.p3[0], markerInfo.p3[1], 64));
+			latlngs.push(ui.mcToLatLng(markerInfo.p4[0], markerInfo.p4[1], 64));
+			latlngs.push(ui.mcToLatLng(markerInfo.p1[0], markerInfo.p1[1], 64));
+			
+			return L.polyline(latlngs, {"color" : markerInfo.color});
+		},
+		"markers" : {
+			"world" : [
+				{
+					"p1" : [42, 0],
+					"p2" : [0, 0],
+					"p3" : [0, 42],
+					"p4" : [42, 42],
+					"color" : "red",
+				},
+				{
+					"p1" : [73, -42],
+					"p2" : [-42, -42],
+					"p3" : [-42, 73],
+					"p4" : [73, 73],
+					"color" : "yellow",
+				},
+			],
+		},
+	},*/
+	
+];
